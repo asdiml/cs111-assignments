af856cd722982b71544d48fcfdcfb4497c738da3
liubrian267 <liubrian267@gmail.com>
2025-05-27 02:04:56 +0000

    6.1 passed read/write/jump tests

--- pintos/threads/thread.h
+++ pintos/threads/thread.h
@@ -90,6 +90,7 @@ struct thread {
     uint8_t *stack; /* Saved stack pointer. */
     int priority; /* Priority. */
     struct list_elem allelem; /* List element for all threads list. */
+    int exit_status; /* Exit status for user processes. */
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem; /* List element. */
